42fd8ddea206015cfd065c7f095c0080793cdd3c
Sebastian Battle <sabattle@eckerd.edu>
2018-06-21 20:48:23 -0400

    trying to fix cat command

--- commands/cat.js
+++ commands/cat.js
@@ -7,7 +7,7 @@ module.exports = {
   description: "Calypso will find a random cat for your viewing pleasure.",
   tag: "fun",
   run: async (bot, message, args) => {
-    let img = (await snekfetch.get("http://aws.random.cat/meow")).body.file;
+    let img = await snekfetch.get("http//random.cat/view").body.file;
     if (!img) return message.channel.send("Yikes, I think the kitties are on the fritz! Try again in a few seconds.")
     let embed = new Discord.RichEmbed()
       .setAuthor("Meow!")
