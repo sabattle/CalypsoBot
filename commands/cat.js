7bef78676b08407863d668772806cb0bff261223
Kyle Glaws <krglaws@eckerd.edu>
2018-06-23 21:56:55 -0400

    fixed cat command

--- commands/cat.js
+++ commands/cat.js
@@ -7,8 +7,25 @@ module.exports = {
   description: "Calypso will find a random cat for your viewing pleasure.",
   tag: "fun",
   run: async (bot, message, args) => {
-    let img = (await snekfetch.get("http://aws.random.cat/meow")).body.file;
+    let img = null;
+    const maxTries = 10;
+    let counter = 0;
+
+    // try to get cat image 10 times
+    while(!img && counter < maxTries){
+        try {
+          img = (await snekfetch.get("http://aws.random.cat/meow")).body.file;
+        } catch (err) {
+          console.log("!cat failed, error: "+err.message);
+          img = null;
+          counter++;
+        }
+    }
+
+    // GET failed, try again later
     if (!img) return message.channel.send("Yikes, I think the kitties are on the fritz! Try again in a few seconds.")
+    
+    // GET was successful, send image to discord
     let embed = new Discord.RichEmbed()
       .setAuthor("Meow!")
       .setImage(img)
