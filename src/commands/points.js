349e347b48a6ab915c7e3677f21fec28450c7be2
Sebastian <sabattle@eckerd.edu>
2018-07-07 21:06:53 -0400

    fixed points command

--- src/commands/points.js
+++ src/commands/points.js
@@ -8,8 +8,9 @@ module.exports = {
   run: async (message, args) => {
     if (message.member.roles.find('name', 'Admin')) {
       let target =  message.mentions.members.first() || message.member;
-      let row = await sqlite.get(`SELECT * FROM db WHERE userId ='${target.id}'`);
-      message.channel.send(`${target.displayName} has **${row.points}** points!`);
+      let row = await sqlite.get(`SELECT * FROM score WHERE userId ='${target.id}'`);
+      if (!row) message.channel.send(`${target.displayName} has **0** points!`);
+      else message.channel.send(`${target.displayName} has **${row.points}** points!`);
     }
     else message.channel.send(`${message.member.displayName}, you do not have permission to use this command!`);
   }
