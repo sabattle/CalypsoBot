42e88ec7301a06e252b0333471700e96135439e2
Sebastian <sabattle@eckerd.edu>
2018-10-01 00:01:29 -0400

    changes to points, need to revisit purge & purgebot

--- src/commands/points.js
+++ src/commands/points.js
@@ -4,9 +4,9 @@ module.exports = {
   name: 'points',
   usage: '<USER MENTION>',
   description: 'Fetches a user\'s current points (or your own points, if no user is mentioned).',
-  tag: 'admin',
+  tag: 'fun',
   run: async (message, args) => {
-    if (message.member.roles.find('name', 'Admin')) {
+    if (message.member.roles.find('name', 'Member')) {
       let target = message.mentions.members.first() || message.member;
       let row = await sqlite.get(`SELECT * FROM ${message.guild} WHERE userId ='${target.id}'`);
       if (!row) message.channel.send(`${target.displayName} has **0** points!`);
--- src/events/message.js
+++ src/events/message.js
@@ -1,15 +1,21 @@
 module.exports = async (client, message) => {
   if (message.channel.type === 'dm' || message.author.bot) return;
   //command handler
+  let command;
   if (message.content.charAt(0) === client.prefix){
     let args = message.content.trim().split(/ +/g);
-    let command = client.commands.get(args.shift().slice(client.prefix.length).toLowerCase());
+    command = client.commands.get(args.shift().slice(client.prefix.length).toLowerCase());
     if (command) command.run(message, args);
   }
   else {
     let reaction = client.reactions.find('prompt', message.content);
     if (reaction) reaction.run(message);
   }
+  //points
   let userId = message.author.id, guild = message.guild;
-  if (message.channel.id != client.devChannelID) require('../updatePoints.js')(userId, guild);
+  if (message.channel.id != client.devChannelID && !command) {
+    if (message.content.includes('http')) require('../updatePoints.js')(userId, guild, 10);
+    else if (message.attachments.size > 0) require('../updatePoints.js')(userId, guild, 20);
+    else require('../updatePoints.js')(userId, guild);
+  }
 }
