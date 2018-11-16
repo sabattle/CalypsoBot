e0c4fda566a50bfe30c05122e13ee74d95b0bcb9
Sebastian Battle <sabattle@eckerd.edu>
2018-11-16 18:21:23 -0500

    added totalpoints command

---
+++ src/commands/totalpoints.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'totalpoints',
+  usage: '<USER MENTION>',
+  description: 'Fetches a user\'s all-time total points (or your own total points, if no user is mentioned).',
+  tag: 'fun',
+  run: async (message) => {
+    const target = message.mentions.members.first() || message.member;
+    const score = message.client.getScore.get(target.id, message.guild.id);
+    if (!score) message.channel.send(`${target.displayName} has **0** all-time points!`);
+    else if (score.totalPoints === 1) message.channel.send(`${target.displayName} has **${score.totalPoints}** all-time point!`);
+    else if (score.totalPoints <= 10000) message.channel.send(`${target.displayName} has **${score.totalPoints}** all-time points!`);
+    else if (score.totalPoints <= 100000) message.channel.send(`${target.displayName} has **${score.totalPoints}** all-time points! Wow, that's a lot!`);
+    else message.channel.send(`${target.displayName} has **${score.totalPoints}** all-time points! Holy guacamole!`);
+  }
+};
