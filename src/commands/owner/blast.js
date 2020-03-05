fd3df29d174de40204e95c1b26c38648b4d1986f
Sebastian <sebbttl@gmail.com>
2020-03-05 18:42:29 -0500

    added blast command

---
+++ src/commands/owner/blast.js
@@ -0,0 +1,23 @@
+const Command = require('../Command.js');
+
+module.exports = class BlastCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'blast',
+      usage: '',
+      description: 'Sends a message to every server that Calypso is in.',
+      type: 'owner',
+      ownerOnly: true
+    });
+  }
+  run(message, args) {
+    const msg = args.join(' ');
+    message.client.guilds.forEach(guild => {
+      const id = message.client.db.guildSettings.selectDefaultChannelId.pluck().get(message.guild.id);
+      let defaultChannel;
+      if (id) defaultChannel = message.guild.channels.get(id);
+      if (defaultChannel) defaultChannel.send(msg);
+      else message.channel.send(`Unable to send message in: ${guild.name}`);
+    });
+  } 
+};
