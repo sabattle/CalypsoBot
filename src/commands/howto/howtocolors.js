aabe52ef13d6c556cf702ec93605498b9c5d2c57
sabattle <sabattle@eckerd.edu>
2020-03-26 00:05:19 -0400

    updated howtocolors command

--- src/commands/howto/howtocolors.js
+++ src/commands/howto/howtocolors.js
@@ -15,7 +15,8 @@ module.exports = class HowToColorsCommand extends Command {
     const prefix = message.client.db.guildSettings.selectPrefix.pluck().get(message.guild.id); // Get prefix
     message.channel.send(
       'Colors can be set up by creating roles beginning with `#` that have specific color hexes. For example, ' +
-      '**#Red** or **#Blue**. These roles should have cleared permissions and be at the bottom of the role hierarchy.' +
+      '**#Red** or **#Blue**. These roles should have cleared permissions and be at the bottom of the role hierarchy. ' +
+      `Alternatively, you can use the \`${prefix}createcolor\` command to easily generate a new color!` +
       `\n\nOnce set, server members can see all available colors by using the command \`${prefix}colors\`.\n\n` +
       oneLine`To choose a specific color, members can use the \`${prefix}color\` command followed by a specific color 
       name or a role mention. For example, \`${prefix}color Red\`.`
