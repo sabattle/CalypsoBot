b5178c65a25516e58188d2c5b9335894b6b53104
Sora <kiuy.or@icloud.com>
2020-06-12 19:07:03 +0200

    Update and rename config.example.json to config.example.js

---
+++ config.example.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const configuration = {
+  token: "your_token_here",
+  ownerID: "your_ID_here",
+  adminsIDS: [],
+  apiKeys: {
+    "catApi": "your_API_key_here",
+    "googleApi": "your_API_key_here"
+  }
+};
+
+exports = configuration;
