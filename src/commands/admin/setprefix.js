13d8ea07ace0f8cb6cb555e04f84871750b8faa0
Sebastian <sebbttl@gmail.com>
2020-06-30 23:47:42 -0400

    updated setprefix command

--- src/commands/admin/setprefix.js
+++ src/commands/admin/setprefix.js
@@ -1,23 +1,37 @@
 const Command = require('../Command.js');
-const { oneLine } = require('common-tags');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class SetPrefixCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'setprefix',
       aliases: ['setp', 'sp'],
-      usage: '<PREFIX>',
-      description: 'Sets the command prefix for your server (max length of 3 characters).',
+      usage: 'setprefix <PREFIX>',
+      description: 'Sets the command prefix for your server. The max prefix length is 3 characters.',
       type: 'admin',
-      userPermissions: ['MANAGE_GUILD']
+      userPermissions: ['MANAGE_GUILD'],
+      examples: ['setprefix !']
     });
   }
   run(message, args) {
+    const oldPrefix = message.client.db.guildSettings.selectPrefix.pluck().get(message.guild.id);
     const prefix = args[0];
-    if (!prefix || prefix.length > 3) return message.channel.send(oneLine`
-      Sorry ${message.member}, I don't recognize that. Please ensure the prefix is no larger than 3 characters.
-    `);
+    if (!prefix) return this.sendErrorMessage(message, 'Invalid argument. Please specify a prefix.');
+    else if (prefix.length > 3) 
+      return this.sendErrorMessage(
+        message, 'Invalid argument. Please ensure the prefix is no larger than 3 characters.'
+      );
     message.client.db.guildSettings.updatePrefix.run(prefix, message.guild.id);
-    message.channel.send(`Successfully updated the \`prefix\` to \`${prefix}\`.`);
+    const embed = new MessageEmbed()
+      .setTitle('Server Settings')
+      .addField('Setting', '**Prefix**', true)
+      .addField('Current Prefix', `\`${oldPrefix}\` 🡪 \`${prefix}\``, true)
+      .setThumbnail(message.guild.iconURL())
+      .setFooter(`
+        Requested by ${message.member.displayName}#${message.author.discriminator}`, message.author.displayAvatarURL()
+      )
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
   }
 };
