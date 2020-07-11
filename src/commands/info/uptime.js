fbc0bdd86216d2968f8dba567f51756b6537a177
Sebastian <sebbttl@gmail.com>
2020-07-11 00:07:47 -0400

    added uptime command

---
+++ src/commands/info/uptime.js
@@ -0,0 +1,31 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+
+module.exports = class UptimeCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'uptime',
+      aliases: ['up'],
+      usage: 'uptime',
+      description: 'Fetches Calypso\'s current uptime.',
+      type: types.INFO
+    });
+  }
+  run(message) {
+    const d = moment.duration(message.client.uptime);
+    const hours = (d.hours() == 1) ? `${d.hours()} hour` : `${d.hours()} hours`;
+    const minutes = (d.minutes() == 1) ? `${d.minutes()} minute` : `${d.minutes()} minutes`;
+    const seconds = (d.seconds() == 1) ? `${d.seconds()} second` : `${d.seconds()} seconds`;
+    const date = moment().subtract(d, 'ms').format('dddd, MMMM Do YYYY');
+    const embed = new MessageEmbed()
+      .setTitle('Calypso\'s Uptime')
+      .setThumbnail('https://raw.githubusercontent.com/sabattle/CalypsoBot/develop/data/images/Calypso.png')
+      .setDescription(`\`${hours}\`, \`${minutes}\`, and \`${seconds}\``)
+      .addField('Date Launched', date) 
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
+  }
+};
