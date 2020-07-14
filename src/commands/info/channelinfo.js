fb348fe960ccaa2928c6fd89da4c789aed2282d2
Sebastian Battle <sebbttl@gmail.com>
2020-07-13 21:20:29 -0400

    added roleinfo and channelinfo commands

---
+++ src/commands/info/channelinfo.js
@@ -0,0 +1,60 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+const { oneLine } = require('common-tags');
+const channelTypes = {
+  dm: 'DM',
+  text: 'Text',
+  voice: 'Voice',
+  category: 'Category',
+  news: 'News',
+  store: 'Store'
+};
+
+module.exports = class ChannelInfoCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'channelinfo',
+      aliases: ['channel', 'ci'],
+      usage: 'channelinfo [channel mention/ID]',
+      description: oneLine`
+        Fetches information about the provided channel. 
+        If no channel is given, the current channel will be used.
+      `,
+      type: client.types.INFO,
+      examples: ['channelinfo #general']
+    });
+  }
+  run(message, args) {
+    let channel = this.getChannelFromMention(message, args[0]) || message.guild.channels.cache.get(args[0]);
+    if (channel) {
+      args.shift();
+    } else channel = message.channel;
+    const embed = new MessageEmbed()
+      .setTitle(channel.name)
+      .setThumbnail(message.guild.iconURL({ dynamic: true }))
+      .addField('Channel ID', `\`${channel.id}\``, true)
+      .addField('Type', `\`${channelTypes[channel.type]}\``, true)
+      .addField('Members', `\`${channel.members.size}\``, true)
+      .addField('Bots', `\`${channel.members.array().filter(b => b.user.bot).length}\``, true)
+      .addField('Created On', moment(channel.createdAt).format('MMM DD YYYY'), true)
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    if (channel.topic) embed.setDescription(channel.topic);
+    if (channel.type === 'text') {
+      embed // Text embed
+        .setTitle(`#${channel.name}`)
+        .spliceFields(2, 0, { name: 'Rate Limit', value: `\`${channel.rateLimitPerUser}\``, inline: true })
+        .spliceFields(5, 0, { name: 'NSFW', value: `\`${channel.nsfw}\``, inline: true })
+    } else if (channel.type === 'voice') {
+      embed // Voice embed
+        .spliceFields(4, 0, { name: 'User Limit', value: `\`${channel.userLimit}\``, inline: true })
+        .spliceFields(5, 0, { name: 'Full', value: `\`${channel.full}\``, inline: true });
+      const members = channel.members.array()
+      if (members.length > 0) 
+        embed.addField('Members Joined', message.client.utils.trimArray(channel.members.array()).join(' '));
+    }
+    message.channel.send(embed);
+  }
+};
---
+++ src/commands/info/roleinfo.js
@@ -0,0 +1,40 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+const permissions = require('../../utils/permissions.json');
+const { oneLine } = require('common-tags');
+
+module.exports = class RoleInfoCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'roleinfo',
+      aliases: ['role', 'ri'],
+      usage: 'roleinfo <role mention/ID>',
+      description: 'Fetches information about the provided role',
+      type: client.types.INFO,
+      examples: ['roleinfo @Member']
+    });
+  }
+  run(message, args) {
+    const role = this.getRoleFromMention(message, args[0]) || message.guild.roles.cache.get(args[0]);
+    if (!role) return this.sendErrorMessage(message, 'Invalid argument. Please mention a role or provide a role ID.');
+    const rolePermissions = role.permissions.toArray().sort((a, b) => {
+      return Object.keys(permissions).indexOf(a) - Object.keys(permissions).indexOf(b);
+    }).map(p => '`' + permissions[p] + '`');
+    const embed = new MessageEmbed()
+      .setTitle(role.name)
+      .setThumbnail(message.guild.iconURL({ dynamic: true }))
+      .addField('Role ID', `\`${role.id}\``, true)
+      .addField('Position', `\`${role.position}\``, true)
+      .addField('Hoisted', `\`${role.hoist}\``, true)
+      .addField('Color', `\`${role.hexColor.toUpperCase()}\``, true)
+      .addField('Members', `\`${role.members.size}\``, true)
+      .addField('Mentionable', `\`${role.mentionable}\``, true)
+      .addField('Created On', moment(role.createdAt).format('MMM DD YYYY'), true)
+      .addField('Permissions', (rolePermissions.length > 0) ? rolePermissions.join(' ') : '`None`')
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(role.hexColor);
+    message.channel.send(embed);
+  }
+};
