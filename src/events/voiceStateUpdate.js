ede9738a6e6fdb245a2172c4ad19afe32b2f06ec
Sebastian <sabattle@eckerd.edu>
2020-07-31 23:49:45 -0400

    updated voiceStateUpdate to v12

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,21 +1,25 @@
-module.exports = (client, oldMember, newMember) => {
+module.exports = (client, oldState, newState) => {
+  
+  // Check member
+  if (oldState.member != newState.member) return;
+  const member = newState.member;
   
   // Get points
   const { point_tracking: pointTracking, voice_points: voicePoints } = 
-    client.db.settings.selectPoints.get(newMember.guild.id);
+    client.db.settings.selectPoints.get(member.guild.id);
   if (!pointTracking || voicePoints == 0) return;
 
   // Set IDs
-  const oldId = oldMember.voiceChannelID;
-  const newId = newMember.voiceChannelID;
-  const afkId = newMember.guild.afkChannelID;
+  const oldId = oldState.channelID;
+  const newId = newState.channelID;
+  const afkId = member.guild.afkChannelID;
 
   if (oldId === newId) return;
   else if ((!oldId || oldId === afkId) && newId && newId !== afkId) { // Joining channel that is not AFK
-    newMember.interval = setInterval(() => {
-      client.db.users.updatePoints.run({ points: voicePoints }, newMember.id, newMember.guild.id);
-    }, 5000);
+    member.interval = setInterval(() => {
+      client.db.users.updatePoints.run({ points: voicePoints }, member.id, member.guild.id);
+    }, 60000);
   } else if (oldId && (oldId !== afkId && !newId || newId === afkId)) { // Leaving voice chat or joining AFK
-    clearInterval(oldMember.interval);
+    clearInterval(member.interval);
   }
 };
