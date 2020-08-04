98bf8ed3b94857863d0a93570d4c52d834848bc0
Sebastian <sabattle@eckerd.edu>
2020-08-03 22:39:15 -0400

    disabled minutes/seconds in crown schedule cron

--- src/commands/admin/setcrownschedule.js
+++ src/commands/admin/setcrownschedule.js
@@ -21,15 +21,15 @@ module.exports = class SetCrownScheduleCommand extends Command {
         │    └─────────────── hour (0 - 23)
         └──────────────────── minute (0 - 59)\`\`\`
         If you wish to use multiple values for any of the categories, please separate them with \`,\`.` +
-        ' Step syntax is also supported, for example: `*/30 * * * *` (every 30 minutes). ' +
+        ' Step syntax is also supported, for example: `0 */1 * * *` (every hour). ' +
         'For the day of the week, both 0 and 7 may represent Sunday. ' +
         'If you need additional help building your cron, please check out this website: <https://crontab.guru/#>. ' + 
         `Enter no schedule to clear the current \`crown schedule\`.
         A \`crown role\` must also be set to enable role rotation.
-        **Please Note:** Not all months have the same amount of days.`,
+        **Please Note:** To prevent potential Discord API abuse, minutes and seconds will always be set to \`0\`.`,
       type: client.types.ADMIN,
       userPermissions: ['MANAGE_GUILD'],
-      examples: ['setcrownschedule 0 21 * * 3,6', 'setcrownschedule * 0 12 /10 * *']
+      examples: ['setcrownschedule 0 21 * * 3,6', 'setcrownschedule 0 0 15 * *']
     });
   }
   run(message, args) {
@@ -43,7 +43,7 @@ module.exports = class SetCrownScheduleCommand extends Command {
     const crownChannel = message.guild.channels.cache.get(crownChannelId);
 
     // Get status
-    const oldStatus = message.client.utils.getStatus(crownRoleId, crownSchedule);
+    const oldStatus = message.client.utils.getStatus(crownRoleId, oldCrownSchedule);
 
     // Trim message
     if (crownMessage) {
@@ -51,10 +51,11 @@ module.exports = class SetCrownScheduleCommand extends Command {
       crownMessage = `\`\`\`${crownMessage}\`\`\``;
     }
 
+    let description = 'The `crown schedule` was successfully updated. <:success:736449240728993802>';
     const embed = new MessageEmbed()
       .setTitle('Settings: `Crown System`')
       .setThumbnail(message.guild.iconURL({ dynamic: true }))
-      .setDescription('The `crown schedule` was successfully updated. <:success:736449240728993802>')
+      .setDescription(description)
       .addField('Role', crownRole || '`None`', true)
       .addField('Channel', crownChannel || '`None`', true)
       .addField('Message', crownMessage || '`None`')
@@ -77,7 +78,7 @@ module.exports = class SetCrownScheduleCommand extends Command {
       );
     }
 
-    const crownSchedule = message.content.slice(message.content.indexOf(args[0]), message.content.length);
+    let crownSchedule = message.content.slice(message.content.indexOf(args[0]), message.content.length);
     try {
       parser.parseExpression(crownSchedule);
     } catch (err) {
@@ -86,6 +87,21 @@ module.exports = class SetCrownScheduleCommand extends Command {
         If you need additional help building your cron, please check out this website: <https://crontab.guru/#>
       `);
     }
+
+    // Set minutes and seconds to 0
+    const cron = crownSchedule.split(' ');
+    if (cron[0] != '0') {
+      description = description + `\n**Note:** Minutes were changed from \`${cron[0]}\` to \`0\`.`;
+      cron[0] = '0';
+    }
+    if (cron.length === 6 && cron[5] != '0') {
+      if (description.includes('\n'))
+        description = description.slice(0, -1) + `, and seconds were changed from \`${cron[5]}\` to \`0\`.`;
+      else description = description + `\n**Note:** Seconds were changed from \`${cron[5]}\` to \`0\`.`;
+      cron[5] = '0';
+    } 
+    crownSchedule = cron.join(' ');
+    embed.setDescription(description);
 
     message.client.db.settings.updateCrownSchedule.run(crownSchedule, message.guild.id);
     if (message.guild.job) message.guild.job.cancel(); // Cancel old job
