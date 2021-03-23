18afc74aac82e2e634b867d34406196625fc1299
Ishita <77601249+krishna-js@users.noreply.github.com>
2021-03-23 14:28:58 +0530

    fixed typo in credits command

--- src/commands/info/credits.js
+++ src/commands/info/credits.js
@@ -18,7 +18,7 @@ module.exports = class CreditsCommand extends Command {
     .addField('IshitaJs',`[Github](https://github.com/ishitajs) + [Discord](https://discord.gg/drunk)`, false)
     .addField('SaBattle', `[Github](https://github.com/sabattle) + [Discord](https://discord.gg/zB9xKAjrHV) `, true)
     .addField('Anogh', `[Github](https://github.com/Anogh297) + [Discord](https://discord.gg/zB9xKAjrHV)`, true)
-    .addField('Hurricano', `[Github](https://github.com/ishitajs/Hurricano) + [Discord](https://discord.gg/tfv7Kw86Rm)`, true)
+    .addField('DragonizedPizza', `[Github](https://github.com/ishitajs/Hurricano) + [Discord](https://discord.gg/tfv7Kw86Rm)`, true)
     .addField('Zerio', `[Github](https://github.com/ZerioDev/Music-bot) + [Discord](https://discord.gg/5cGSYV8ZZj)`, true)
     .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
     .setTimestamp()
