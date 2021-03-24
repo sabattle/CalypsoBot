6449ebf5adbbefc0812f3f68993a7fc06a418ccb
Ishita <78543191+ishitajs@users.noreply.github.com>
2021-03-24 11:06:03 +0530

    installed google-play-scrapper its shit uninstalled in a second subecting multiple errors in all files idk y

--- src/commands/fun/pp.js
+++ src/commands/fun/pp.js
@@ -12,15 +12,16 @@ module.exports = class PpCommand extends Command {
     });
   }
 
-run(msg, args) {
-    let user = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]) || msg.member
+  run(msg, args) {
+    let user = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]) || msg.member;
     if (this.client.isOwner(user)) {
-        if (user.id === msg.author.id) return msg.channel.send(`**${user.displayName}'s Size:**\n8D`);
-        return msg.channel.send(`**${user.displayName}'s Size:**\n8=D`);
+      if (user.id === msg.author.id) return msg.channel.send(`**${user.displayName}'s Size:**\n8D`);
+      return msg.channel.send(`**${user.displayName}'s Size:**\n8=D`);
     }
     const clientAuthor = user.id === this.client.user.id;
     const random = MersenneTwister19937.seed(clientAuthor ? msg.author.id : user.id);
     const length = integer(0, 50)(random);
     return msg.channel.send(`**${user.displayName}'s Size:**\n8${'='.repeat(clientAuthor ? length + 1 : length)}D`);
 
-}}
+  }
+};
