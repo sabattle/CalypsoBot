2410f32981503b90fd2ab6cae585801a833d2494
ishitajs <78543191+ishitajs@users.noreply.github.com>
2021-04-09 15:41:26 +0530

    Readded the code

--- src/events/messageReactionRemove.js
+++ src/events/messageReactionRemove.js
@@ -1,29 +1,50 @@
-const { MessageReaction, User } = require("discord.js");
-let db = require("quick.db");
-let Discord = require("discord.js");
-let { MessageEmbed } = require("discord.js");
+module.exports = async (client, messageReaction, user) => {
 
-module.exports = async (client, reaction, user) => {
-  let message = reaction.message;
-  let roleObject = db.fetch(`rolereactions_${message.guild.id}_${message.id}`);
-  let emoji = reaction.emoji.toString();
-  if (roleObject) {
-    if (emoji === roleObject.emoji) {
-      let member = message.guild.members.cache.get(user.id);
-      let role = message.guild.roles.cache.get(roleObject.role);
-      if (!member.roles.cache.has(role)) {
-        try {
-          member.roles.remove(role);
-                   const removed = new MessageEmbed()
-        .setTitle(`❗ Removed A Role In \`${reaction.message.guild.name}\``)
-        .addField(`**Role**`, `${role.name}`)
-        .setColor('RED')
-        .setAuthor(`${reaction.message.guild.name}`, reaction.message.guild.iconURL({dynamic: true}))
-          member.send(removed)
-        } catch (e) {
-          message.channel.send("Something has gone wrong " + e.message);
-        }
-      }
+  if (client.user === user) return;
+
+  const { message, emoji } = messageReaction;
+
+  // Starboard
+  if (emoji.name === '⭐' && message.author != user) {
+    const starboardChannelId = client.db.settings.selectStarboardChannelId.pluck().get(message.guild.id);
+    const starboardChannel = message.guild.channels.cache.get(starboardChannelId);
+    if (
+      !starboardChannel || 
+      !starboardChannel.viewable ||
+      !starboardChannel.permissionsFor(message.guild.me).has(['SEND_MESSAGES', 'EMBED_LINKS']) ||
+      message.channel === starboardChannel
+    ) return;
+
+    const emojis = ['⭐', '🌟', '✨', '💫', '☄️'];
+    const messages = await starboardChannel.messages.fetch({ limit: 100 });
+
+    const starred = messages.find(m => {
+      return emojis.some(e => {
+        return m.content.startsWith(e) &&
+          m.embeds[0] &&
+          m.embeds[0].footer &&
+          m.embeds[0].footer.text == message.id;
+      });
+    });
+
+    // If message already in starboard
+    if (starred) {
+
+      const starCount = parseInt(starred.content.split(' ')[1].slice(2)) - 1;
+
+      // Determine emoji type
+      let emojiType;
+      if (starCount > 20) emojiType = emojis[4];
+      else if (starCount > 15) emojiType = emojis[3];
+      else if (starCount > 10) emojiType = emojis[2];
+      else if (starCount > 5) emojiType = emojis[1];
+      else emojiType = emojis[0];
+
+      const starMessage = await starboardChannel.messages.fetch(starred.id);
+      await starMessage.edit(`${emojiType} **${starCount}  |**  ${message.channel}`);
+
+      if (starCount == 0)
+        await starMessage.delete().catch(err => client.logger.error(err.stack));
     }
   }
 };
