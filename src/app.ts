f288ca32422286a8575ae7e9479576d3bc316ccb
Sebastian Battle <sebastian.battle@proton.me>
2022-10-29 00:38:20 -0400

    feat: actually start the bot

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-import { Events, GatewayIntentBits } from 'discord.js'
+import { GatewayIntentBits } from 'discord.js'
 import config from 'config'
 import Client from 'structures/Client'
 
 const client = new Client(config, { intents: [GatewayIntentBits.Guilds] })
 
-client.once(Events.ClientReady, () => {
-  console.log('Ready!')
-})
+;(async (): Promise<void> => await client.init())()
