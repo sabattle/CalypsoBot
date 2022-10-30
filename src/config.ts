ce9d5aca2363194ece2d344ef00343609ee2df8e
Sebastian Battle <sebastian.battle@proton.me>
2022-10-30 13:54:54 -0400

    refactor: update config

--- src/config.ts
+++ src/config.ts
@@ -4,13 +4,7 @@ import { getEnvironmentVariable } from 'utils'
 config()
 
 export default {
-  clientId:
-    process.env.NODE_ENV == 'production'
-      ? getEnvironmentVariable('CLIENT_ID')
-      : getEnvironmentVariable('DEV_CLIENT_ID'),
-  token:
-    process.env.NODE_ENV == 'production'
-      ? getEnvironmentVariable('TOKEN')
-      : getEnvironmentVariable('DEV_TOKEN'),
+  token: getEnvironmentVariable('TOKEN'),
+  clientId: getEnvironmentVariable('CLIENT_ID'),
   guildId: getEnvironmentVariable('GUILD_ID'),
 }
